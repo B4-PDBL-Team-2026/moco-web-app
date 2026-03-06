9f40b94b4030f9e7ee87def8001dc785bea30055
yosev <yosmisyael7308@gmail.com>
2026-03-06 10:18:45 +0700

    feat(auth): add controller for reset password page

--- app/Http/Controllers/Web/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Web/Auth/ResetPasswordController.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace App\Http\Controllers\Web\Auth;
+
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use \App\Http\Controllers\Controller;
+use Inertia\Response;
+
+class ResetPasswordController extends Controller
+{
+    public function showResetForm(Request $request, string $token): Response
+    {
+        return Inertia::render('Auth/AuthPassword', [
+            'token' => $token,
+            'email' => $request->query('email'),
+        ]);
+    }
+}
