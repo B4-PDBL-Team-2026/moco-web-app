1405b7a9680b97291b1fd70a78fab333cf0dc9e0
yosev <yosmisyael7308@gmail.com>
2026-03-16 02:58:09 +0700

    test(config): apply test case on unit

--- tests/Pest.php
+++ tests/Pest.php
@@ -7,17 +7,10 @@
 |
 | The closure you provide to your test functions is always bound to a specific PHPUnit test
 | case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
-| need to change it using the "uses()" function to bind a different classes or traits.
+| need to change it using the "pest()" function to bind a different classes or traits.
 |
 */
 
-// Cukup gunakan satu blok ini saja untuk semua folder di bawah 'Feature'
-uses(
-    Tests\TestCase::class,
-    Illuminate\Foundation\Testing\RefreshDatabase::class
-)->in('Feature');
-
-/*
-|--------------------------------------------------------------------------
-| Expectations ... (sisanya biarkan saja)
-*/
+pest()->extend(Tests\TestCase::class)
+    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->in('Feature', 'Unit');
