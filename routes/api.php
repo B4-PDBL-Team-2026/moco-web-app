ce3641c2de325ef101c0c2866af6b2540b701d32
yosev <yosmisyael7308@gmail.com>
2026-03-16 20:04:50 +0700

    refactor(api): adjust implementation of route registration for transaction api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\TransactionController;
 use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\Onboarding\OnboardingController;
+use App\Http\Controllers\Api\Transaction\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
@@ -16,22 +16,18 @@ Route::prefix('auth')->group(function () {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
+    // Onboarding Endpoints
     Route::controller(OnboardingController::class)->prefix('onboarding')->group(function () {
         Route::get('/', 'show');
         Route::post('/', 'store');
     });
+
+    // Transaction Endpoints
+    Route::controller(TransactionController::class)->prefix('transaction')->group(function () {
+        Route::get('/transactions', 'index');
+        Route::post('/transactions', 'store');
+        Route::get('/transactions/{transaction}', 'show');
+        Route::put('/transactions/{transaction}', 'update');
+        Route::delete('/transactions/{transaction}', 'destroy');
+    });
 });
-
-Route::middleware('auth:sanctum')->group(function () {
-
-    Route::get('/transactions', [TransactionController::class, 'index']);
-    Route::post('/transactions', [TransactionController::class, 'store']);
-    Route::get('/transactions/{id}', [TransactionController::class, 'show']);
-    Route::put('/transactions/{id}', [TransactionController::class, 'update']);
-    Route::delete('/transactions/{id}', [TransactionController::class, 'destroy']);
-
-});
-
-Route::get('/verify-email/{id}/{hash}', [AuthController::class, 'verifyEmail'])
-    ->middleware(['signed'])
-    ->name('verification.verify');
