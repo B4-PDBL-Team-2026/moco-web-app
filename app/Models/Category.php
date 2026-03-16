2777d603866ef4f32add597007665238b891f89d
yosev <yosmisyael7308@gmail.com>
2026-03-16 19:51:27 +0700

    feat(category): add category typecolumn

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Enums\TransactionType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany; // untuk testing
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class ExpenseCategory extends Model
+class Category extends Model
 {
     use HasFactory;
 
@@ -15,9 +16,14 @@ class ExpenseCategory extends Model
 
     protected $fillable = [
         'name',
+        'type',
         'icon',
         'colors',
         'user_id',
+    ];
+
+    protected $casts = [
+        'type' => TransactionType::class,
     ];
 
     public function user(): BelongsTo
