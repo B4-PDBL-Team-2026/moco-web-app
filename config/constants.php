edfb44f00dc11e30b34e3870a7c7d51397e02b3e
yosev <yosmisyael7308@gmail.com>
2026-03-19 03:27:22 +0700

    feat(categories): add app default categories

--- config/constants.php
+++ config/constants.php
@@ -1,6 +1,26 @@
 <?php
 
 return [
-    'expenseCategories' => ['Makan', 'Transport', 'Kos', 'Kuliah', 'Hiburan', 'Belanja', 'Kesehatan', 'Lainnya'],
-    'incomeCategories' => ['Uang saku', 'Gaji', 'Hadiah', 'Investasi', 'Pinjaman', 'Transfer Masuk', 'Lainnya'],
+    'expenseCategories' => [
+        'Makan' => 'bowl_food',
+        'Transport' => 'taxi',
+        'Tagihan' => 'invoice',
+        'Edukasi' => 'student',
+        'Hiburan' => 'film_reel',
+        'Belanja' => 'shopping_bag',
+        'Kesehatan' => 'heart_beat',
+        'Donasi' => 'hand_heart',
+        'Lainnya' => 'tag',
+    ],
+
+    'incomeCategories' => [
+        'Uang saku' => 'money',
+        'Gaji' => 'wallet',
+        'Refund' => 'hand_coins',
+        'Hadiah' => 'gift',
+        'Investasi' => 'chart_line_up',
+        'Pinjaman' => 'arrow_circle_down',
+        'Transfer Masuk' => 'arrow_circle_right',
+        'Lainnya' => 'tag',
+    ],
 ];
