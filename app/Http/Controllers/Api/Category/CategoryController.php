cee4540134ecd9a8fa608f425166eebf1e0fe702
yosev <yosmisyael7308@gmail.com>
2026-03-23 16:16:34 +0700

    feat(category): implement caching in read system categories operation

---
+++ app/Http/Controllers/Api/Category/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\Category;
+
+use App\Domains\Category\Actions\GetSystemCategoriesAction;
+use App\Traits\ApiResponse;
+use Illuminate\Http\JsonResponse;
+
+class CategoryController
+{
+    use ApiResponse;
+    public function getAllSystemCategory(GetSystemCategoriesAction $action): JsonResponse
+    {
+        $result = $action->execute();
+
+        return $this->success($result, 'Retrieved all system category successfully.');
+    }
+}
