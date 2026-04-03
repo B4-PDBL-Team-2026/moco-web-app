5814b3329697e64e81b97494ae865ce25f69a75a
yosev <yosmisyael7308@gmail.com>
2026-04-03 15:56:27 +0700

    test(auth): make test pass

--- tests/Feature/Http/Controllers/Web/Auth/ResetPasswordWebTest.php
+++ tests/Feature/Http/Controllers/Web/Auth/ResetPasswordWebTest.php
@@ -5,12 +5,12 @@ use Inertia\Testing\AssertableInertia as Assert;
 test('it can render reset password page correctly', function () {
     $this->withoutVite();
 
-    $response = $this->get('/reset-password/token-rahasia-123?email=test@moco.com');
+    $response = $this->get('auth/reset-password/token-rahasia-123?email=test@moco.com');
 
     $response->assertStatus(200);
 
     $response->assertInertia(fn (Assert $page) => $page
-        ->component('Auth/AuthPassword')
+        ->component('Auth/ResetPassword')
         ->where('token', 'token-rahasia-123')
         ->where('email', 'test@moco.com')
     );
