e0f8f82ff1afb54f8b330d32b5e86294cdd7adfa
yosev <yosmisyael7308@gmail.com>
2026-04-03 15:49:50 +0700

    refactor(auth): grouped auth web endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\Web\Auth\AuthController;
 use App\Http\Controllers\Web\Auth\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::prefix('auth')->group(function () {
+    Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::get('/verify-email/{id}/{hash}', [AuthController::class, 'verifyEmail'])
+        ->middleware(['signed'])
+        ->name('verification.verify');
+});
