bdc14f14ded66e01369ef6866d6019e1a0650f2c
Dinda Diyah Arifa <dindadiyaharifa@gmail.com>
2026-04-04 11:28:00 +0700

    fix(transactions): ensure transactions are ordered by transaction_date

--- app/Domains/Transactions/Actions/GetAllTransactionAction.php
+++ app/Domains/Transactions/Actions/GetAllTransactionAction.php
@@ -15,15 +15,11 @@ class GetAllTransactionAction
             ->when($data->month, fn ($query) => $query->whereMonth('transaction_date', $data->month))
             ->when($data->year, fn ($query) => $query->whereYear('transaction_date', $data->year))
             ->when($data->search, fn ($query) => $query->where('name', 'like', "%{$data->search}%"))
-<<<<<<< HEAD
             ->when($data->categoryId, fn ($query) => $query
                 ->where('category_id', $data->categoryId)
                 ->when($data->categoryType, fn ($query) => $query->where('category_type', $data->categoryType))
             )
-=======
-            ->when($data->categoryId, fn ($query) => $query->where('category_id', $data->categoryId))
             ->latest('transaction_date')
->>>>>>> d8096e3 (fix(transactions): ensure transactions are ordered by transaction_date)
             ->latest()
             ->paginate($data->perPage);
 
