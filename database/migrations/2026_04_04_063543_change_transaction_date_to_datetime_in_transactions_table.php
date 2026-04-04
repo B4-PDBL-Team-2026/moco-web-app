ea239442f436b00b00f111dce69a5aa1211304cd
yosev <yosmisyael7308@gmail.com>
2026-04-04 15:33:25 +0700

    feat(transaction): update column name into transaction_at and change to datetime of transactions table

---
+++ database/migrations/2026_04_04_063543_change_transaction_date_to_datetime_in_transactions_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Models\Transaction;
+use App\Models\UserBudgetSetting;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->renameColumn('transaction_date', 'transaction_at');
+        });
+
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dateTime('transaction_at')->change();
+        });
+
+        Transaction::chunkById(500, function ($transactions) {
+            foreach ($transactions as $transaction) {
+
+                $userTimezone = UserBudgetSetting::where('user_id', $transaction->user_id)->value('timezone') ?? 'UTC';
+
+                $dateString = CarbonImmutable::parse($transaction->transaction_date)->format('Y-m-d');
+
+                $safeLocalTime = CarbonImmutable::createFromFormat(
+                    'Y-m-d H:i:s',
+                    $dateString.' 12:00:00',
+                    $userTimezone
+                );
+
+                $transaction->update([
+                    'transaction_date' => $safeLocalTime->utc()->toDateTimeString(),
+                ]);
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->date('transaction_date')->change();
+        });
+    }
+};
