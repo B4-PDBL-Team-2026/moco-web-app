e48dfa469d6d864a4d20778fe19728f866ddb04a
yosev <yosmisyael7308@gmail.com>
2026-04-25 03:22:46 +0700

    feat(config): publish localization config

---
+++ lang/id/errors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'budget' => [
+        'ceiling_too_low' => 'Waduh, limit atas kamu nggak boleh lebih kecil dari limit bawah ya. Yuk, disesuaikan lagi angkanya!',
+        'budget_setting_not_found' => 'Kamu belum setup pengaturan budgeting kamu, setup dulu yuk!',
+        'balance_insufficient' => 'Yah, saldo kamu kurang nih buat transaksi ini. Pikir lagi yuk!',
+    ],
+];
