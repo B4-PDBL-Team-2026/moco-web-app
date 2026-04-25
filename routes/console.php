9f70fdf080bb96fe2ad9e8344cbd0e227b68117c
yosev <yosmisyael7308@gmail.com>
2026-04-25 18:28:21 +0700

    refactor(notification): remove unused command

--- routes/console.php
+++ routes/console.php
@@ -1,11 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
-
-Schedule::command('fixed-cost:remind')->dailyAt('08:00');
+Schedule::command('fixed-cost:remind')
+    ->timezone('Asia/Jakarta')
+    ->dailyAt('08:00');
