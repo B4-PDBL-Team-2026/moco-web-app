cd749e32f0e9fd2edff3c73721233607e55fde6c
yosev <yosmisyael7308@gmail.com>
2026-04-25 10:08:32 +0700

    feat(config): add local error message

--- lang/id/errors.php
+++ lang/id/errors.php
@@ -1,9 +1,18 @@
 <?php
 
 return [
+    'authorization' => [
+        'not_authorized' => 'Eits, fitur ini bukan buat kamu. Cek akses akunmu lagi, yuk!',
+    ],
     'budget' => [
         'ceiling_too_low' => 'Waduh, limit atas kamu nggak boleh lebih kecil dari limit bawah ya. Yuk, disesuaikan lagi angkanya!',
         'budget_setting_not_found' => 'Kamu belum setup pengaturan budgeting kamu, setup dulu yuk!',
-        'balance_insufficient' => 'Yah, saldo kamu kurang nih buat transaksi ini. Pikir lagi yuk!',
+        'balance_insufficient' => 'Eits, langkah ini bisa bikin saldo kamu minus. Biar catatan keuanganmu tetap sehat, sesuaikan nominal atau transaksinya dulu, ya!',
+    ],
+    'transactions' => [
+        'future_date' => 'Sabar dulu! Tanggal transaksi nggak boleh di masa depan. Pakai tanggal hari ini atau sebelumnya, ya.',
+    ],
+    'category' => [
+        'mismatch' => 'Kategorinya nggak nyambung, nih. Pastikan kategori yang kamu pilih sesuai dengan jenis transaksinya, ya!',
     ],
 ];
