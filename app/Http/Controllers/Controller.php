727fc7103dc569fba0e69f20246c97467339a542
yosev <yosmisyael7308@gmail.com>
2026-05-02 01:31:15 +0700

    feat(app): implement response wrapper methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Responses\ApiResponse;
+
 abstract class Controller
 {
-    //
+    protected function successResponse(mixed $data = null, string $message = 'Success', int $status = 200): ApiResponse
+    {
+        return ApiResponse::success(data: $data, message: $message, status: $status);
+    }
+
+    protected function errorResponse(string $message = 'Error', int $status = 400, mixed $errors = null): ApiResponse
+    {
+        return ApiResponse::error(errors: $errors, message: $message, status: $status);
+    }
 }
