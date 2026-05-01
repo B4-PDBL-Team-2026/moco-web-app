a6e49ac67d072ed74c32308aaa31e36c41595f6f
yosev <yosmisyael7308@gmail.com>
2026-05-02 01:33:45 +0700

    refactor(notification): reimplement handler method and add doc

--- app/Http/Controllers/Api/Notification/InAppNotificationController.php
+++ app/Http/Controllers/Api/Notification/InAppNotificationController.php
@@ -1,37 +1,74 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Api\Notification;
 
 use App\Http\Controllers\Controller;
-use App\Traits\ApiResponse;
+use App\Http\Resources\Notification\NotificationResource;
+use App\Http\Responses\ApiResponse;
 
 class InAppNotificationController extends Controller
 {
-    use ApiResponse;
-
-    public function index()
+    /**
+     * Get list of notifications.
+     *
+     * @response array{
+     *     success: bool,
+     *     message: string,
+     *     data: array<NotificationResource>,
+     *     meta?: array{
+     *         currentPage: int,
+     *         lastPage: int,
+     *         perPage: int,
+     *         total: int,
+     *         hasMore: bool
+     *     }
+     * }
+     */
+    public function index(): ApiResponse
     {
         $user = auth()->user();
 
         $notifications = $user->notifications()->paginate(20);
 
-        return $this->success($notifications, 'Notification list retrieved successfully.');
+        return $this->successResponse(
+            data: NotificationResource::collection($notifications),
+            message: 'Notifications retrieved successfully.',
+        );
     }
 
-    public function markAsRead($id)
+    /**
+     * Mark a notification item has been read
+     *
+     * @param  string  $id  ID of notification
+     *
+     * @response array{success: bool, message: string}
+     */
+    public function markAsRead(string $id): ApiResponse
     {
         $user = auth()->user();
 
         $notification = $user->notifications()->findOrFail($id);
         $notification->markAsRead();
 
-        return $this->success(message: 'Notification marked as read.');
+        return $this->successResponse(
+            message: 'Notification marked as read.',
+        );
     }
 
+    /**
+     * Get total of unread notifications
+     *
+     * @response array{success: bool, message: string, data: array{total: int}}
+     */
     public function getUnreadTotal()
     {
-        return $this->success([
-            'total' => auth()->user()->unreadNotifications->count(),
-        ]);
+        return $this->successResponse(
+            data: [
+                'total' => auth()->user()->unreadNotifications->count(),
+            ],
+            message: 'Unread notification total retrieved successfully.',
+        );
     }
 }
