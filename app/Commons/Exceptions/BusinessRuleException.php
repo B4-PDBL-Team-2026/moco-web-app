bcf101447aa06ec120ebc94aa8ea4cc2212d73b9
yosev <yosmisyael7308@gmail.com>
2026-05-02 01:31:50 +0700

    feat(app): change domain exception implementation

--- app/Commons/Exceptions/BusinessRuleException.php
+++ app/Commons/Exceptions/BusinessRuleException.php
@@ -2,6 +2,31 @@
 
 namespace App\Commons\Exceptions;
 
-use Exception;
+use RuntimeException;
 
-class BusinessRuleException extends Exception {}
+class BusinessRuleException extends RuntimeException
+{
+    public function __construct(
+        protected readonly string $translationKey,
+        protected readonly array $translationParams = [],
+        protected readonly int $httpStatus = 422,
+    ) {
+        logger()->info('bre called');
+        parent::__construct($this->translationKey);
+    }
+
+    public function getTranslationKey(): string
+    {
+        return $this->translationKey;
+    }
+
+    public function getTranslationParams(): array
+    {
+        return $this->translationParams;
+    }
+
+    public function getHttpStatus(): int
+    {
+        return $this->httpStatus;
+    }
+}
