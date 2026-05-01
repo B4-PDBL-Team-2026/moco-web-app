2c134264c3d31864f7ecea256f63d650117d2498
yosev <yosmisyael7308@gmail.com>
2026-05-02 02:32:51 +0700

    feat(auth): create auth resource

---
+++ app/Http/Resources/Auth/AuthResource.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Resources\Auth;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'user' => $this['user'],
+            'token' => $this['token'],
+            'requiresOnboarding' => $this['requiresOnboarding'],
+        ];
+    }
+}
