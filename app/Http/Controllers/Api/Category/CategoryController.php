096fc3d72a71cca61fb6cdd3269930571aa4d1ed
yosev <yosmisyael7308@gmail.com>
2026-05-02 15:19:50 +0700

    refactor(category): implement response wrapper for all handler

--- app/Http/Controllers/Api/Category/CategoryController.php
+++ app/Http/Controllers/Api/Category/CategoryController.php
@@ -3,17 +3,25 @@
 namespace App\Http\Controllers\Api\Category;
 
 use App\Domains\Category\Actions\GetSystemCategoriesAction;
-use App\Traits\ApiResponse;
-use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Category\CategoryResource;
+use App\Http\Responses\ApiResponse;
 
-class CategoryController
+class CategoryController extends Controller
 {
-    use ApiResponse;
-
-    public function getAllSystemCategory(GetSystemCategoriesAction $action): JsonResponse
+    /**
+     * Get all system categories
+     *
+     * @response array{
+     *     success: bool,
+     *     message: string,
+     *     data: array<CategoryResource>
+     * }
+     */
+    public function getAllSystemCategory(GetSystemCategoriesAction $action): ApiResponse
     {
         $result = $action->execute();
 
-        return $this->success($result, 'Retrieved all system category successfully.');
+        return $this->successResponse(CategoryResource::collection($result), 'Retrieved all system category successfully.');
     }
 }
