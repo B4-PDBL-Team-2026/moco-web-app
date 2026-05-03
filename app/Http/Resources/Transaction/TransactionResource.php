c1d198f923e83e82ea3a3138dcfc998dc80d9bc1
yosev <yosmisyael7308@gmail.com>
2026-05-03 10:48:04 +0700

    feat(transaction): implement transaction resource

---
+++ app/Http/Resources/Transaction/TransactionResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources\Transaction;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'amount' => $this->amount,
+            'type' => $this->type,
+            'source' => $this->source,
+            'note' => $this->note,
+            'transactionAt' => $this->transaction_at?->toIso8601String(),
+            'category' => $this->whenLoaded('category', function () {
+                return [
+                    'id' => $this->category->id,
+                    'name' => $this->category->name,
+                    'icon' => $this->category->icon ?? null,
+                ];
+            }),
+        ];
+    }
+}
