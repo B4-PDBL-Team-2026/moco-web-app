ce733958e0602b0f3ae5b17f749c2083b1e87d7b
yosev <yosmisyael7308@gmail.com>
2026-05-03 10:48:57 +0700

    refactor(transaction): remove wildcard prefix for better like query performance

--- app/Domains/Transaction/Actions/GetAllTransactionAction.php
+++ app/Domains/Transaction/Actions/GetAllTransactionAction.php
@@ -14,7 +14,10 @@ class GetAllTransactionAction
             ->where('user_id', $userId)
             ->when($data->month, fn ($query) => $query->whereMonth('transaction_at', $data->month))
             ->when($data->year, fn ($query) => $query->whereYear('transaction_at', $data->year))
-            ->when($data->search, fn ($query) => $query->where('name', 'like', "%{$data->search}%"))
+            ->when($data->search, function ($query) use ($data) {
+                $operator = $query->getConnection()->getDriverName() === 'pgsql' ? 'ilike' : 'like';
+                $query->where('name', $operator, "{$data->search}%");
+            })
             ->when($data->categoryId, fn ($query) => $query
                 ->where('category_id', $data->categoryId)
             )
