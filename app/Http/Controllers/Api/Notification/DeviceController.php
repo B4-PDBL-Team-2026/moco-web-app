e60fa8091c537e476e44ba93e4ccb51edd96d819
yosev <yosmisyael7308@gmail.com>
2026-05-03 10:31:23 +0700

    feat(notification): implement response wrapper

--- app/Http/Controllers/Api/Notification/DeviceController.php
+++ app/Http/Controllers/Api/Notification/DeviceController.php
@@ -5,22 +5,21 @@ namespace App\Http\Controllers\Api\Notification;
 use App\Domains\Notification\Actions\RegisterUserDeviceAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Notification\RegisterDeviceRequest;
-use App\Traits\ApiResponse;
-use Illuminate\Http\JsonResponse;
+use App\Http\Responses\ApiResponse;
 
 class DeviceController extends Controller
 {
-    use ApiResponse;
-
     /**
      * Register user device for push mobile notification
+     *
+     * @response array{success: bool, message: string}
      */
     public function registerDevice(
         RegisterDeviceRequest $request,
         RegisterUserDeviceAction $action
-    ): JsonResponse {
+    ): ApiResponse {
         $action->execute(auth()->user(), $request->toDTO());
 
-        return $this->success(message: 'Device registered successfully.');
+        return $this->successResponse(message: 'Device registered successfully.');
     }
 }
