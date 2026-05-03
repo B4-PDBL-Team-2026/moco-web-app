d1b39cf5043aa745adda2f5025c059b70785e1c6
yosev <yosmisyael7308@gmail.com>
2026-05-03 10:12:51 +0700

    feat(fixed cost): implement resource for fixed cost template

---
+++ app/Http/Resources/FixedCost/FixedCostTemplateResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources\FixedCost;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FixedCostTemplateResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'amount' => $this->amount,
+            'cycleType' => $this->cycle_type,
+            'dueDay' => $this->due_day,
+            'isActive' => (bool) $this->is_active,
+        ];
+    }
+}
