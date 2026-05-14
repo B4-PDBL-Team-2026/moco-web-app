3e3eb0936f0a08f0de46a59b00ff2eb1d25820b2
yosev <yosmisyael7308@gmail.com>
2026-05-15 02:41:32 +0700

    refactor(notification): add return type

--- app/Domains/Notification/Actions/GetAllRegisteredDeviceAction.php
+++ app/Domains/Notification/Actions/GetAllRegisteredDeviceAction.php
@@ -3,10 +3,11 @@
 namespace App\Domains\Notification\Actions;
 
 use App\Domains\User\Models\UserDevice;
+use Illuminate\Support\Collection;
 
 class GetAllRegisteredDeviceAction
 {
-    public function execute(int $userId)
+    public function execute(int $userId): Collection
     {
         return UserDevice::query()
             ->where('user_id', $userId)
