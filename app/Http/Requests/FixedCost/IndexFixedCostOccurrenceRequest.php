920bcdef7bbfd0a6a0ca78ee06066c3371873d76
yosev <yosmisyael7308@gmail.com>
2026-05-15 03:52:13 +0700

    refactor(category): move request validation into their domain

--- app/Http/Requests/FixedCost/IndexFixedCostOccurrenceRequest.php
+++ app/Http/Requests/FixedCost/IndexFixedCostOccurrenceRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\FixedCost;
 
 use App\Domains\FixedCost\DTOs\FilterFixedCostOccurrenceData;
 use App\Domains\FixedCost\Enums\FixedCostOccurenceStatus;
