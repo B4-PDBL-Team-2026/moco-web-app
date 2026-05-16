b47d4957d507a8c5c19f1be097c2b0b734ab4c62
yosev <yosmisyael7308@gmail.com>
2026-05-16 14:48:19 +0700

    refactor(transaction): move dto conversion into http layer

--- app/Domains/Transaction/DTOs/UpdateTransactionData.php
+++ app/Domains/Transaction/DTOs/UpdateTransactionData.php
@@ -22,28 +22,4 @@ final readonly class UpdateTransactionData
         public bool $transactionAtProvided,
         public ?CarbonImmutable $transactionAt,
     ) {}
-
-    public static function fromArray(array $data): self
-    {
-        return new self(
-            nameProvided: array_key_exists('name', $data),
-            name: $data['name'] ?? null,
-
-            amountProvided: array_key_exists('amount', $data),
-            amount: array_key_exists('amount', $data) ? (string) $data['amount'] : null,
-
-            categoryIdProvided: array_key_exists('categoryId', $data),
-            categoryId: array_key_exists('categoryId', $data) && $data['categoryId'] !== null
-                ? (int) $data['categoryId']
-                : null,
-
-            noteProvided: array_key_exists('note', $data),
-            note: $data['note'] ?? null,
-
-            transactionAtProvided: array_key_exists('transactionAt', $data),
-            transactionAt: array_key_exists('transactionAt', $data)
-                ? CarbonImmutable::parse($data['transactionAt'])->utc()
-                : null,
-        );
-    }
 }
