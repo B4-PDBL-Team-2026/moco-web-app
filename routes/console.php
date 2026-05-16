d5a9446c258b55ec10921cc8ab4c0d935090d735
yosev <yosmisyael7308@gmail.com>
2026-05-16 20:52:08 +0700

    feat(notification): add push notification schedule

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('fixed-cost:remind')
     ->timezone('Asia/Jakarta')
     ->dailyAt('08:00');
+
+Schedule::command('fixed-cost:remind')
+    ->timezone('Asia/Jakarta')
+    ->dailyAt('14:00');
+
+Schedule::command('fixed-cost:remind')
+    ->timezone('Asia/Jakarta')
+    ->dailyAt('19:00');
