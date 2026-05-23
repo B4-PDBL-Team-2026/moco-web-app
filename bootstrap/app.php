58678e66b9a209ab55bb59e749f7cf913332f503
yosev <yosmisyael7308@gmail.com>
2026-05-23 10:09:44 +0700

    feat(api:auth): implement notOnboarded middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Commons\Exceptions\BusinessRuleException;
 use App\Console\Commands\SendFixedCostReminders;
 use App\Http\Middleware\CheckDailyBudgetRecalculation;
 use App\Http\Middleware\EnsureOnboardingIsCompleted;
+use App\Http\Middleware\EnsureOnboardingIsNotCompleted;
 use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Responses\ApiResponse;
 use Illuminate\Auth\AuthenticationException;
@@ -31,6 +32,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->alias([
             'hasOnboarded' => EnsureOnboardingIsCompleted::class,
+            'notOnboarded' => EnsureOnboardingIsNotCompleted::class,
             'hasRecaculatedToday' => CheckDailyBudgetRecalculation::class,
         ]);
     })
