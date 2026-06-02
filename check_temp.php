df30d5fcb4c417027f71a2ae9681a9029cde337a
yosmisyael <yosmisyael7308@gmail.com>
2026-06-02 10:45:09 +0700

    chore: remove unused files

--- check_temp.php
+++
@@ -1,8 +0,0 @@
-<?php
-use App\Domains\User\Models\User;
-
-$users = User::all();
-echo 'User count: ' . $users->count() . PHP_EOL;
-foreach ($users as $u) {
-    echo 'ID: ' . $u->id . ' | Email: ' . $u->email . ' | Onboarded: ' . ($u->has_onboarded ? 'yes' : 'no') . PHP_EOL;
-}
